b50b42ad10bf654a64114a6faf873a720415c94b
illia <illiakrang@gmail.com>
2025-04-10 16:21:05 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DepartmentLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
